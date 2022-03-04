8db7c3d93b6bf11208539ddd8eed3f9272558bc3
David Gemmell <2559087G@student.gla.ac.uk>
2022-03-04 15:52:29 +0000

    Created the html files, updated the urls.py file to include paths and made a start on the base.html template

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,12 @@ app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
 path('about/', views.about, name='about'),
+path('login/', views.login, name="login"),
+path('register/', views.register, name="register"),
+path('logout/', views.logout, name="logout"),
+path('account/', views.account, name="account"),
+path('search/', views.search, name="search"),
+path('add_post/', views.add_post, name="add_post"),
+#not sure about this last line will need to figure this out
+path('post/<int:id>', views.show_post, name="show_post"),
 ]
